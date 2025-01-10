d8c0427f5cd89c975a78cbd1540f2dc8d1940c7c
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2025-01-10 19:48:08 -0300

    Re-testing My Bugs updates

--- src/app/view1/view1.ts
+++ src/app/view1/view1.ts
@@ -1,8 +1,8 @@
 import { html, css, LitElement } from 'lit';
 import { customElement, state, query } from 'lit/decorators.js';
-import { defineComponents, IgcAvatarComponent, IgcBadgeComponent, IgcBannerComponent, IgcButtonComponent, IgcButtonGroupComponent, IgcCalendarComponent, IgcCardComponent, IgcChipComponent, IgcDatePickerComponent, IgcDialogComponent, IgcIconButtonComponent, IgcIconComponent, IgcInputComponent, IgcListComponent, IgcListItemComponent, IgcRadioComponent, IgcRadioGroupComponent, IgcRatingComponent, IgcRippleComponent, IgcSelectComponent, IgcSliderComponent, IgcSnackbarComponent, IgcStepperComponent, IgcSwitchComponent, IgcTabsComponent, IgcTextareaComponent, IgcToggleButtonComponent, IgcTreeComponent } from 'igniteui-webcomponents';
+import { defineComponents, IgcAvatarComponent, IgcBadgeComponent, IgcBannerComponent, IgcButtonComponent, IgcButtonGroupComponent, IgcCalendarComponent, IgcCardComponent, IgcChipComponent, IgcDatePickerComponent, IgcDialogComponent, IgcIconButtonComponent, IgcIconComponent, IgcInputComponent, IgcRadioComponent, IgcRadioGroupComponent, IgcRatingComponent, IgcRippleComponent, IgcSelectComponent, IgcSliderComponent, IgcSnackbarComponent, IgcStepperComponent, IgcSwitchComponent, IgcTabsComponent, IgcTextareaComponent, IgcToggleButtonComponent, IgcTreeComponent } from 'igniteui-webcomponents';
 
-defineComponents(IgcButtonComponent, IgcRippleComponent, IgcButtonGroupComponent, IgcToggleButtonComponent, IgcChipComponent, IgcIconComponent, IgcSwitchComponent, IgcRadioGroupComponent, IgcRadioComponent, IgcIconButtonComponent, IgcRatingComponent, IgcBadgeComponent, IgcAvatarComponent, IgcTreeComponent, IgcBannerComponent, IgcDialogComponent, IgcCardComponent, IgcTabsComponent, IgcListComponent, IgcListItemComponent, IgcStepperComponent, IgcInputComponent, IgcSelectComponent, IgcDatePickerComponent, IgcTextareaComponent, IgcSliderComponent, IgcCalendarComponent, IgcSnackbarComponent);
+defineComponents(IgcButtonComponent, IgcRippleComponent, IgcButtonGroupComponent, IgcToggleButtonComponent, IgcChipComponent, IgcIconComponent, IgcSwitchComponent, IgcRadioGroupComponent, IgcRadioComponent, IgcIconButtonComponent, IgcRatingComponent, IgcAvatarComponent, IgcBadgeComponent, IgcTreeComponent, IgcBannerComponent, IgcDialogComponent, IgcCardComponent, IgcTabsComponent, IgcStepperComponent, IgcInputComponent, IgcSelectComponent, IgcDatePickerComponent, IgcTextareaComponent, IgcSliderComponent, IgcCalendarComponent, IgcSnackbarComponent);
 
 @customElement('app-view1')
 export default class View1 extends LitElement {
@@ -259,28 +259,10 @@ export default class View1 extends LitElement {
     .avatar_3::part(base) {
       background-color: transparent;
     }
-    .avatar_4::part(base) {
-      background-color: transparent;
-    }
-    .avatar_5::part(base) {
-      background-color: transparent;
-    }
     .avatar_1_1::part(base) {
       background-color: transparent;
     }
-    .avatar_1_2::part(base) {
-      background-color: transparent;
-    }
-    .avatar_1_3::part(base) {
-      background-color: transparent;
-    }
     .avatar_2_1::part(base) {
-      background-color: transparent;
-    }
-    .avatar_2_2::part(base) {
-      background-color: transparent;
-    }
-    .avatar_2_3::part(base) {
       background-color: transparent;
     }
     .badge {
@@ -295,10 +277,6 @@ export default class View1 extends LitElement {
     .tree {
       min-width: max-content;
       min-height: max-content;
-      flex-shrink: 0;
-    }
-    .list {
-      height: max-content;
       flex-shrink: 0;
     }
   `;
@@ -424,42 +402,41 @@ export default class View1 extends LitElement {
           <igc-rating label="Rating Component" value="${this.value1}" @igcChange="${(e: any) => this.value1 = e.detail}" class="user-input"></igc-rating>
         </div>
         <div class="row-layout group_6">
-          <igc-badge class="badge">
-            Badge
-          </igc-badge>
-          <igc-badge class="badge">
-            1
-          </igc-badge>
-          <igc-badge class="badge">
-            10
-          </igc-badge>
-          <igc-badge class="badge">
-            1000
-          </igc-badge>
-          <igc-badge class="badge">
+          <igc-avatar shape="circle" class="avatar">
             <span class="material-icons">
-              mail
+              people
             </span>
-          </igc-badge>
-        </div>
-        <div class="row-layout group_6">
-          <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" shape="circle" class="avatar avatar_3"></igc-avatar>
-          <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" shape="circle" class="avatar_1 avatar_1_1"></igc-avatar>
-          <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" shape="circle" class="avatar_2 avatar_2_1"></igc-avatar>
-          <igc-avatar initials="AB" shape="circle" class="avatar"></igc-avatar>
-          <igc-avatar initials="AB" shape="circle" class="avatar_1"></igc-avatar>
-          <igc-avatar initials="AB" shape="circle" class="avatar_2"></igc-avatar>
-        </div>
-        <div class="row-layout group_6">
-          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar avatar_4"></igc-avatar>
+          </igc-avatar>
           <igc-badge class="badge">
             Badge
           </igc-badge>
-          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar_1 avatar_1_2"></igc-avatar>
+          <igc-avatar shape="circle" class="avatar_1">
+            <span class="material-icons">
+              people
+            </span>
+          </igc-avatar>
           <igc-badge class="badge">
             Badge
           </igc-badge>
-          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar_2 avatar_2_2"></igc-avatar>
+          <igc-avatar shape="circle" class="avatar_2">
+            <span class="material-icons">
+              people
+            </span>
+          </igc-avatar>
+          <igc-badge class="badge">
+            Badge
+          </igc-badge>
+        </div>
+        <div class="row-layout group_6">
+          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar avatar_3"></igc-avatar>
+          <igc-badge class="badge">
+            Badge
+          </igc-badge>
+          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar_1 avatar_1_1"></igc-avatar>
+          <igc-badge class="badge">
+            Badge
+          </igc-badge>
+          <igc-avatar src="/src/assets/Avatar3.png" shape="circle" class="avatar_2 avatar_2_1"></igc-avatar>
           <igc-badge class="badge">
             Badge
           </igc-badge>
@@ -559,34 +536,7 @@ export default class View1 extends LitElement {
             </span>
             <span>Label</span>
           </igc-tab>
-          <igc-tab-panel class="row-layout tab-item-content">
-            <igc-list class="list">
-              <igc-list-item>
-                <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" slot="start" shape="circle" class="avatar avatar_5"></igc-avatar>
-                <div slot="title">Title goes here</div>
-                <div slot="subtitle">Subtitle...</div>
-                <span slot="end" class="material-icons icon_2">
-                  star
-                </span>
-              </igc-list-item>
-              <igc-list-item>
-                <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" slot="start" shape="circle" class="avatar_1 avatar_1_3"></igc-avatar>
-                <div slot="title">Title goes here</div>
-                <div slot="subtitle">Subtitle...</div>
-                <span slot="end" class="material-icons icon_2">
-                  star
-                </span>
-              </igc-list-item>
-              <igc-list-item>
-                <igc-avatar src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39..v1605559903.png" slot="start" shape="circle" class="avatar_2 avatar_2_3"></igc-avatar>
-                <div slot="title">Title goes here</div>
-                <div slot="subtitle">Subtitle...</div>
-                <span slot="end" class="material-icons icon_2">
-                  star
-                </span>
-              </igc-list-item>
-            </igc-list>
-          </igc-tab-panel>
+          <igc-tab-panel class="row-layout tab-item-content"></igc-tab-panel>
           <igc-tab>
             <span class="material-icons">
               info
--- src/app/view3/view3.ts
+++ src/app/view3/view3.ts
@@ -1,5 +1,8 @@
 import { html, css, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { defineComponents, IgcButtonComponent, IgcCardComponent, IgcIconButtonComponent, IgcIconComponent, IgcRippleComponent } from 'igniteui-webcomponents';
+
+defineComponents(IgcCardComponent, IgcButtonComponent, IgcRippleComponent, IgcIconButtonComponent, IgcIconComponent);
 
 @customElement('app-view3')
 export default class View3 extends LitElement {
@@ -11,11 +14,185 @@ export default class View3 extends LitElement {
       align-items: stretch;
       align-content: flex-start;
     }
+    .column-layout {
+      display: flex;
+      flex-direction: column;
+    }
+    .group {
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+      gap: 16px;
+      position: relative;
+      padding: 16px;
+      min-width: 50px;
+      min-height: 50px;
+      flex-grow: 1;
+      flex-basis: 0;
+    }
+    .card {
+      height: max-content;
+    }
+    .media-content {
+      height: 200px;
+      min-height: 200px;
+      max-height: 200px;
+    }
+    .body-content {
+      min-width: 50px;
+      min-height: 50px;
+    }
+    .actions-content {
+      min-width: 50px;
+      min-height: 40px;
+    }
+    .image {
+      height: 100%;
+      min-height: 100%;
+      max-height: 100%;
+    }
+    .text {
+      height: max-content;
+      min-width: min-content;
+    }
+    .button {
+      --ig-size: var(--ig-size-medium);
+      height: max-content;
+    }
   `;
 
   render() {
     return html`
+      <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
       <link rel='stylesheet' href='../../ig-theme.css'>
+      <div class="column-layout group">
+        <igc-card class="card">
+          <igc-card-media class="media-content">
+            <img src="/src/assets/design%20system.png" class="image" />
+          </igc-card-media>
+          <igc-card-header>
+            <h3 slot="title">
+              Title goes here...
+            </h3>
+            <h5 slot="subtitle">
+              Subtitle goes here...
+            </h5>
+          </igc-card-header>
+          <igc-card-content class="body-content">
+            <p class="typography__body-2 text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur soluta nulla asperiores, officia ullam recusandae voluptatem omnis perferendis vitae non magni magnam praesentium placeat nemo quas repudiandae. Nisi, quo ex!
+            </p>
+          </igc-card-content>
+          <igc-card-actions class="actions-content">
+            <igc-button variant="flat" type="button" slot="start" class="button">
+              Button
+              <igc-ripple></igc-ripple>
+            </igc-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                favorite
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                bookmark
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                share
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+          </igc-card-actions>
+        </igc-card>
+        <igc-card class="card">
+          <igc-card-media class="media-content">
+            <img src="/src/assets/design%20system.png" class="image" />
+          </igc-card-media>
+          <igc-card-header>
+            <h3 slot="title">
+              Title goes here...
+            </h3>
+            <h5 slot="subtitle">
+              Subtitle goes here...
+            </h5>
+          </igc-card-header>
+          <igc-card-content class="body-content">
+            <p class="typography__body-2 text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur soluta nulla asperiores, officia ullam recusandae voluptatem omnis perferendis vitae non magni magnam praesentium placeat nemo quas repudiandae. Nisi, quo ex!
+            </p>
+          </igc-card-content>
+          <igc-card-actions class="actions-content">
+            <igc-button variant="flat" type="button" slot="start" class="button">
+              Button
+              <igc-ripple></igc-ripple>
+            </igc-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                favorite
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                bookmark
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                share
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+          </igc-card-actions>
+        </igc-card>
+        <igc-card class="card">
+          <igc-card-media class="media-content">
+            <img src="/src/assets/design%20system.png" class="image" />
+          </igc-card-media>
+          <igc-card-header>
+            <h3 slot="title">
+              Title goes here...
+            </h3>
+            <h5 slot="subtitle">
+              Subtitle goes here...
+            </h5>
+          </igc-card-header>
+          <igc-card-content class="body-content">
+            <p class="typography__body-2 text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur soluta nulla asperiores, officia ullam recusandae voluptatem omnis perferendis vitae non magni magnam praesentium placeat nemo quas repudiandae. Nisi, quo ex!
+            </p>
+          </igc-card-content>
+          <igc-card-actions class="actions-content">
+            <igc-button variant="flat" type="button" slot="start" class="button">
+              Button
+              <igc-ripple></igc-ripple>
+            </igc-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                favorite
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                bookmark
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+            <igc-icon-button slot="end" variant="flat">
+              <span class="material-icons">
+                share
+              </span>
+              <igc-ripple></igc-ripple>
+            </igc-icon-button>
+          </igc-card-actions>
+        </igc-card>
+      </div>
     `;
   }
 }
